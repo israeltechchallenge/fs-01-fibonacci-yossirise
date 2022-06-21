f938fc705ae8c8424ae12a914f495989d2b13c00
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-06-21 10:11:31 +0300

    Do milestone 2

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const fibonacci_index = 7,
-  fibonacci_number = 8;
+  fibonacci_number = fibonacci(fibonacci_index);
 
 document.querySelector(".fibonacci-index").textContent = fibonacci_index;
 document.querySelector(".fibonacci-number").textContent = fibonacci_number;
+
+function fibonacci(index) {
+  if (index <= 1) {
+    return 0;
+  }
+
+  let prev = 0,
+    curr = 1;
+
+  for (let i = 3; i <= index; i++) {
+    [prev, curr] = [curr, prev + curr];
+  }
+
+  return curr;
+}
